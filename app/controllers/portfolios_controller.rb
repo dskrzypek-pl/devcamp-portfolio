c9720d0b537d8b811aec6945366bdc19056b0c77
Dariusz <dskrzypek@nicmangroup.com>
2018-09-21 12:09:48 +0200

    Implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,7 +40,18 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
 
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: "Post was removed." }
+    end
+  end
 
 
 end
